f7e7a95aa0ec4cdc95f6d97accbf37f1b0f6ed52
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-06-22 20:10:41 -0400

    2. Setting up API

---
+++ src/api.js
@@ -0,0 +1,36 @@
+// Base URL 
+const base_url = `https://api.rawg.io/api/`;
+const key = `key=${process.env.REACT_APP_RAWG_API}`;
+
+// Getting the date 
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if(month < 10){
+        return `0${month}`
+    }else{
+        return month;
+    }
+};
+
+const getCurrentDay = () => {
+    const day = new Date().getDate();
+    if(day < 10){
+        return `0${day}`
+    }else{
+        return day;
+    }
+};
+
+//Current Day/Month/Year 
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+
+// Popular Games
+const popular_games = `games?${key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+const popularGamesUrl = () => `${base_url}${popular_games}`;
