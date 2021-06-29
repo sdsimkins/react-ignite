2e1aae038d53968be9b521147d08038fe7488337
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-06-28 20:24:07 -0400

    6. Fetching other games

--- src/api.js
+++ src/api.js
@@ -32,7 +32,10 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
 const popular_games = `games?${key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?${key}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?${key}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
-
-//This is a git test
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
