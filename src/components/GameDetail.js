4fa504311cf65936d5943e4813b3abe1fb5440e7
SteveDylan <>
2021-07-20 19:21:25 +0000

    18. added icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,13 @@ import {motion} from "framer-motion";
 import {useSelector} from "react-redux";
 import {useHistory} from "react-router-dom";
 import {smallImage} from "../util";
+// Images 
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from '../img/gamepad.svg';
 
 const GameDetail = ({pathId}) => {
   const history = useHistory();
@@ -17,6 +24,25 @@ const GameDetail = ({pathId}) => {
       history.push("/");
     }
   };
+
+  // Get platform images 
+
+  const getPlatform = (platform) => {
+    switch(platform){
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC": 
+        return steam;
+      case "Nintendo Switch": 
+        return nintendo;
+      case "iOS": 
+        return apple;
+      default:
+        return gamepad;
+    }
+  }
   
   // Data 
   const {screen, game, isLoading} = useSelector((state) => state.detail);
@@ -34,7 +60,11 @@ const GameDetail = ({pathId}) => {
             <h3>Platforms</h3>
             <Platforms>
               {game.platforms.map(data => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
+                <img 
+                    key={data.platform.id} 
+                    src={getPlatform(data.platform.name)} 
+                    alt={data.platform.name}>
+                </img>
               ))}
             </Platforms>
           </Info>
